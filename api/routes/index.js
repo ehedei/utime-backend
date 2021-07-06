b2e657924f48d3cb63525d0cd23865b2fe39fad2
ehedei <ehedeidev@gmail.com>
2021-07-06 12:30:40 +0100

    merge

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,14 +1,10 @@
-const { userRouter } = require('./user.router')
-
 const router = require('express').Router()
+const { userRouter } = require('./user.router')
 const { bookingRouter } = require('./booking.router')
 const { appointmentRouter } = require('./appointment.router')
 const { doctorRouter } = require('./doctor.router')
-<<<<<<< HEAD
 const { specialtyRouter } = require('./specialty.router')
-=======
 const { authRouter } = require('./auth.router')
->>>>>>> c25d6ee6242bd094bb709812447493a2a05f74e5
 
 router
   .use('/auth', authRouter)
